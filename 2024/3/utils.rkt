6260c5d7878de8ee2ef8451d77b98c7cc23522a3
Eric Festa <34201007+ericfesta2@users.noreply.github.com>
2024-12-10 22:45:15 -0500

    Replace nested car/cdr usage with nested pattern matching

--- 2024/3/utils.rkt
+++ 2024/3/utils.rkt
@@ -3,10 +3,12 @@
 (define (extract_values_to_mult str)
     (regexp-match* #rx"mul\\(([0-9]+),([0-9]+)\\)" #:match-select values str))
 
+; matches is a list of '(<ORIGINAL_STRING> <CAPTURED_NUM_STR_1> <CAPTURED_NUM_STR_2>)
+; based on the regex in extract_values_to_mult
 (define (sum_multiplied matches sum_so_far)
     (match matches
-        [(cons h t) (sum_multiplied t
-            (+ sum_so_far (* (string->number (cadr h)) (string->number (caddr h)))))]
+        [(cons (list _ op1 op2) t) (sum_multiplied t
+            (+ sum_so_far (* (string->number op1) (string->number op2))))]
         ['() sum_so_far]))
 
 (provide extract_values_to_mult)
