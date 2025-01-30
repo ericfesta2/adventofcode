4ac2bffa75377613938b3c54aaf82507f3d20d32
Eric Festa <34201007+ericfesta2@users.noreply.github.com>
2025-01-29 19:42:48 -0500

    Complete part 2 of day 11

--- 2024/11/part_1_2.kts
+++ 2024/11/part_1_2.kts
@@ -4,7 +4,7 @@ import kotlin.math.pow
 
 private val input = "2 77706 5847 9258441 0 741 883933 12".split(" ").map { it.toLong() }
 
-private val NUM_BLINKS = 25
+private val NUM_BLINKS = 75
 private val DEFAULT_VALUE_MULTIPLIER = 2024
 
 private tailrec fun numDigits(value: Long, count: Int = 0): Int =
@@ -30,10 +30,30 @@ private fun changeStone(stoneValue: Long): List<Long> =
         }
     }
 
+@Deprecated("""
+    The below method worked for part 1 but ran out of memory for part 2 since it did not cache repeated calculations
+    (e.g., two stones in the list with the same value)
+""")
 private fun List<Long>.blinkTimes(times: Int) =
     (0..<times).fold(this) { list, _ ->
         list.flatMap { changeStone(it) }
     }
 
-// Part 1 - works well enough for 25 blinks but won't for 75
-input.blinkTimes(NUM_BLINKS).let { println(it.size) }
+// To solve part 2, for each blink, count the frequencies of each distinct stone value in the list, then compute
+// each distinct new value once; the total number of stones is summed only at the end of the computation.
+private tailrec fun numStones(stoneValueFreqs: Map<Long, Long>, times: Int): Long =
+    if (times == 0) {
+        stoneValueFreqs.values.sum()
+    } else {
+        val newStoneFreqs = mutableMapOf<Long, Long>().withDefault { 0L }
+
+        stoneValueFreqs.forEach { (origStone, origStoneFreq) ->
+            changeStone(origStone).forEach { newStone ->
+                newStoneFreqs.put(newStone, newStoneFreqs.getOrDefault(newStone, 0) + origStoneFreq)
+            }
+        }
+
+        numStones(newStoneFreqs, times - 1)
+    }
+
+println(numStones(input.groupingBy { it }.eachCount().mapValues { it.value.toLong() }, NUM_BLINKS))
